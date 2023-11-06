149512c781a2c1f2834655870f74f87525a8c3f3
root <vereshak.vadim.2004@gmail.com>
2023-11-06 20:11:30 +0200

    test

--- test/app.test.js
+++ test/app.test.js
@@ -13,7 +13,7 @@ describe('POST /sum', () => {
       .send({ num1: 5, num2: 3 })
       .end((err, res) => {
         expect(res).to.have.status(200);
-        expect(res.body).to.have.property('result', 6);
+        expect(res.body).to.have.property('result', 7);
         done();
       });
   });
